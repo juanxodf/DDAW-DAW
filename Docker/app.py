c4989b25b4319777c73a72ebefc2ce6094515391
juanxodf <martinpozuelojuan@gmail.com>
2025-10-24 09:17:08 +0200

    test: probando pre-commit

--- Docker/app.py
+++ Docker/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-def home():
-return    "Hola desde Flask dentro de Docker!" 
+def                     home():
+    return    "Hola desde Flask dentro de Docker!" 
 if __name__=="__main__":
- app.run(host="0.0.0.0",port=8000)  
+app.run(host="0.0.0.0",port=8000)  
